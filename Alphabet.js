2dfd79e427f3b0c5a59932ce5281b6e87da2b56b
nickloverde <nick.loverde@gmail.com>
2021-03-23 10:06:56 -0500

    added solutions

--- Alphabet.js
+++ Alphabet.js
@@ -11,3 +11,19 @@
 // Then, I want to create a switch statement with all the letters corresponding to their alphabet position
 // Then, I want to join them all again as one long String
 
+function alphabetPosition(text) {
+    return text.toLowerCase().split('')
+          .filter( c => c >= 'a' & c <= 'z' )
+          .map( c => c.charCodeAt(0) - 'a'.charCodeAt(0) + 1)
+          .join(' ');
+  }
+
+  function alphabetPosition(text) {
+    let result = [];
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    text = text.replace(/\W*\d+/g, '').toLowerCase().split('');
+    for (let i = 0; i < text.length; i++) {
+        let j = alphabet.indexOf(text[i]) + 1;
+        if (j) result.push(j);
+      }
+  }
