7cb527dae5f7a55bb5b3d23a9000fc7c9ce87a45
nickloverde <nick.loverde@gmail.com>
2021-03-23 09:48:59 -0500

    whiteboarded problem

---
+++ Alphabet.js
@@ -0,0 +1,13 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Whiteboard:
+// First, I want to split each character in the String
+// Then, I want to create a switch statement with all the letters corresponding to their alphabet position
+// Then, I want to join them all again as one long String
+
